ba7243e522efb6640d61181c88b7e282f8f27357
Jesse de Vries <jessedvrs@gmail.com>
2015-06-28 20:31:37 +0200

    Improved documentation

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
 # Meteor Intent Package
 
+Intent lets your [Meteor](https://github.com/meteor/meteor) application remind a user's initial action or page over several route changes (client-side only). Designed for use with [iron:router](https://github.com/iron-meteor/iron-router).
+
 ## Prerequisites
 
 - `meteor-platform` (obviously)
 - `iron:router` or `meteorhacks:flow-router`
 
-## Basic usage
+## Installation
 
-- Install the package using `meteor add lifelynl:intent`.
-- Configure the Intent using `Intent.configure({ fallback_route_name: 'home' })`. Possible configuration options are:
+- `meteor add lifelynl:intent`.
+- Configure the Intent on the client using `Intent.configure({ default_route_name: 'home' })`. Possible configuration options are:
 
 ```
-fallback_route_name       Required    The name of the route where Intent should fall back to
-fallback_route_params     Optional    The parameters for the route where Intent should fall back to
-fallback_route_options    Optional    The query and hash for the route where Intent should fall back to
+default_route_name       Required    The name of the route where Intent should fall back to
+default_route_params     Optional    The parameters for the route where Intent should fall back to
+default_route_options    Optional    The options for the route where Intent should fall back to
+debug                    Optional    Enable debugging
 ```
+
+## Example application
+
+The example application enclose the most common cases for Intent. To view the example application:
+- Checkout the repo with `git clone`
+- `cd meteor-intent/examples`
+- `meteor run`
+- visit [localhost:3000](http://localhost:3000)
+
+## Documentation
+
+Please visit the [wiki on Github](https://github.com/lifelynl/meteor-intent/wiki/)!
--- examples/client/pages/home/home.js
+++ examples/client/pages/home/home.js
@@ -2,19 +2,41 @@
 * Helpers for home
 */
 Template.home.helpers({
-    examples: function() {
+    cases: function() {
         return [
             {
                 title: 'Automatic page return',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, sunt.'
+                description: 'The user simply wants to login, but does not want to lose his current location in the application.',
+                steps: [
+                    'Log out if you\'re logged in',
+                    'Go to Articles',
+                    'Click login',
+                    'Login (register if necessary)',
+                    'You\'re back at the Articles page!'
+                ]
             },
             {
                 title: 'Automatic page return with action',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, sunt.'
+                description: 'The user wants to post a comment on a nice article, but that action requires a user account. He already typed out his comment, and doesn\'t want to lose this.',
+                steps: [
+                    'Log out if you\'re logged in',
+                    'Go to Articles > About the Meteor meetup at Lifely',
+                    'Add a comment',
+                    'Login',
+                    'There\'s your comment!'
+                ]
             },
             {
                 title: 'Deeply inherited intents',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, sunt.'
+                description: 'Even when the user is far away from his initial action, the user should always be brought back and the action should always be completed.',
+                steps: [
+                    'Log out if you\'re logged in',
+                    'Go to Articles > About the Meteor meetup at Lifely',
+                    'Add a comment',
+                    'Click register',
+                    'Create an account',
+                    'There\'s your comment!'
+                ]
             }
         ];
     }
