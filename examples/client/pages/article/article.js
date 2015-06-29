97250c20d1551d7dbd6c647079924cb446128c34
Jesse de Vries <jessedvrs@gmail.com>
2015-06-29 11:55:56 +0200

    Example: added highlight to added comment

--- examples/client/pages/article/article.js
+++ examples/client/pages/article/article.js
@@ -1,3 +1,11 @@
+Template.article.onRendered(function() {
+    this.highlightComment = function(comment_id) {
+        var comment_element_id = '#comment-' + comment_id;
+        location.hash = comment_element_id;
+        $(comment_element_id).addClass('comment-highlight'); // can't use template.find here because by the time this gets ran, template is out of rendered DOM range
+    };
+});
+
 Template.article.helpers({
     commentUser: function() {
         return Meteor.users.findOne(this.user_id);
@@ -12,7 +20,9 @@ Template.article.events({
         var comment = form.elements.comment.value;
 
         var proceed = function() {
-            Meteor.call('articles.comment', template.data.article._id, comment);
+            Meteor.call('articles.comment', template.data.article._id, comment, function(error, comment_id) {
+                template.highlightComment(comment_id);
+            });
             form.reset();
         };
 
@@ -21,8 +31,7 @@ Template.article.events({
             Intent.go({
                 route: 'login'
             }, function(user) {
-                if (!user) return;
-                proceed();
+                if (user) proceed();
             });
 
         } else {
